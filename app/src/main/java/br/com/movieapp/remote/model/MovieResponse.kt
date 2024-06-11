2087016f0ffc4fdddc74a729b8771e937a771819
Raquel Dev <ramariadeveloper@gmail.com>
2024-06-11 14:35:37 -0300

    Refactor model with SerializedName and added proguard config

--- app/src/main/java/br/com/movieapp/remote/model/MovieResponse.kt
+++ app/src/main/java/br/com/movieapp/remote/model/MovieResponse.kt
@@ -2,9 +2,11 @@ package br.com.movieapp.remote.model
 
 import br.com.movieapp.remote.response.MovieResult
 
+import com.google.gson.annotations.SerializedName
+
 data class MovieResponse(
-    val page: Int,
-    val results: List<MovieResult>,
-    val total_pages: Int,
-    val total_results: Int
+    @SerializedName("page") val page: Int,
+    @SerializedName("results") val results: List<MovieResult>,
+    @SerializedName("total_pages") val totalPages: Int,
+    @SerializedName("total_results") val totalResults: Int
 )
