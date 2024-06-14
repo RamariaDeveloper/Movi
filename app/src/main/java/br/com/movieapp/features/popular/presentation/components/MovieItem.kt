bbb118f3eed791f9a7cfec400f08511888089c15
Raquel Dev <ramariadeveloper@gmail.com>
2024-06-14 16:48:10 -0300

    Adjusts MovieItem and added MovieContent

--- app/src/main/java/br/com/movieapp/features/popular/presentation/components/MovieItem.kt
+++ app/src/main/java/br/com/movieapp/features/popular/presentation/components/MovieItem.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,10 +12,16 @@ import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
+import br.com.movieapp.R
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 
 @Composable
 fun MovieItem(
@@ -46,7 +53,34 @@ fun MovieItem(
             shape = RoundedCornerShape(8.dp),
             elevation = 8.dp
         ) {
-
+            Box {
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .crossfade(true)
+                        .error(R.drawable.ic_error_image)
+                        .placeholder(R.drawable.ic_placeholder)
+                        .build(),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillHeight,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.BottomCenter)
+                        .background(Color.Black)
+                        .clip(RoundedCornerShape(8.dp))
+                )
+            }
         }
     }
+}
+
+@Preview
+@Composable
+fun MovieItemPreview() {
+    MovieItem(
+        modifier = Modifier,
+        voteAvarage = 7.2,
+        imageUrl = "",
+        id = 1,
+        onClick = {}
+    )
 }
